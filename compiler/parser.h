28e3766cc98340747608b57ad8d52c70798bd6d3
RoyalGhost <kieranf1913@gmail.com>
2017-03-07 19:51:18 -0800

    Delete parser.h

--- compiler/parser.h
+++
@@ -1,9 +0,0 @@
-#include  "lex.h"
-
-static int layoutChar(char c) {
-    switch (ch) {
-        case ' ': case '\n': case '\t': return 1;
-        default:                        return 0;
-    }
-}
-
