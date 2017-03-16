fe47ff087f853d4cee9dde20c36d02bbea8b0a03
RoyalGhost <kieranf1913@gmail.com>
2017-03-15 17:13:19 -0700

    Create Tokens.py

---
+++ interpreter/Tokens.py
@@ -0,0 +1,10 @@
+# This file just defines the token types.
+# That's it.
+# Just the token types.
+ADD, SUBTRACT = 'ADD', 'SUBTRACT'
+MULTIPLY, DIVIDE = 'MULTIPLY', 'DIVIDE'
+SET = 'SET'
+SELECT = 'SELECT'
+BEGIN, END = 'BEGIN', 'END'
+INTEGER = 'INTEGER'
+WHITESPACE = 'WHITESPACE'
