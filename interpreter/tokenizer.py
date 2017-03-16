847090dcc6436f59d641ea0153dbacc823311a98
RoyalGhost <kieranf1913@gmail.com>
2017-03-15 17:11:02 -0700

    Update tokenizer.py
    Will finish later.

--- interpreter/tokenizer.py
+++ interpreter/tokenizer.py
@@ -1,135 +1,77 @@
-# Token types
-# EOF is used for end of file (duh).
-EOF, INTEGER = 'EOF', 'INTEGER'
-# Mathematical operators
+from ExprHandlers import MathHandler
+import Driver
+
+''' Constants '''
+# END is obviously the end of the compiled list
+# And BEGIN is obviously the start of the compiled list
+BEGIN, END = 'BEGIN', 'END'
+# INTEGER is just the symbol for a number
+INTEGER = 'INTEGER'
+# Here we have the mathematics constants
 PLUS, MINUS = 'PLUS', 'MINUS'
-TIMES, DIVIDE = 'TIMES', 'DIVIDE'
-# Stack prefix. To select a stack and slot, you use the format: 
-# $<stack number<stack slot
-STACK_PREFIX = '$'
-# < is used whenever you are selecting something.
-# Ususally a stack and a slot on the stack.
-SELECT_PREFIX = '<'
-# Assign
-ASSIGN = '='
+MULTIPLY, DIVIDE = 'MULTIPLY', 'DIVIDE'
+# The prefixes
+# So selecting a stack and its slot looks like this:
+# $1~1 for stack 1, slot 1, or
+# $3~4 for stack 3, slot 4
+STACK_PREFIX = 'STACK PREFIX'
+SLOT_PREFIX = 'SLOT PREFIX'
+# Assignment operator
+ASSIGN = 'ASSIGN'
+# Layout character
+LAYOUT = 'LAYOUT'
 # End of statement
-END_OF_STATEMENT = ';'
+EOS = 'EOS'
 
 class Token(object):
+    '''
+    The token class. The list that the tokenizer creates from a text file is made up of these.
+    '''
     def __init__(self, type, value):
-        # Token type: one of the ones defined at the start
         self.type = type
-        # Token value: 0-9, +, -, *. /, or $
         self.value = value
-    
+        
     def __str__(self):
-        '''
-        A string representation of the token.
-        eg:
-        Token(INTEGER, 1), or
-        Token(STACK_PREFIX, 2)
-        '''
-        return 'Token({type}, {value})'.format(
-            type = self.type
-            value = self.value
+        # Returns a string representation of the token.
+        # For example: 
+        # T(INTEGER, 1), or
+        # T(PLUS, +)
+        return 'T({type}, {value})'.format (
+            type = self.type,
+            value = repr(self.value)
         )
         
     def __repr__(self):
         return self.__str__()
         
-
 class Tokenizer(object):
+    '''
+    The tokenizer. This is what 'compiles' or translates the text into a list of tokens.
+    '''
     def __init__(self, text):
-        # String input
         self.text = text
-        # An index onto self.text
+        # Index onto text
         self.pos = 0
-        # Current token instance
+        # Current token
         self.currentToken = None
-        # List of errors
-        self.errors = {1: 'Error: Expected token does not match current token.'}
-        # List of tokens in the file
-        self.tokenList = [Token] * len(self.text)
+        # Token list
+        # We start it with the begin token
+        self.tokenList = [[[None] for x in (Driver.Stacks)] for y in Driver.Slots]
         
-    def error(errorId):
-        raise Exception(self.errors[errorId])
-        
-    def getNextToken(self):
+    def compileToList(self):
         '''
-        Tokenizer, lexer, scanner, whatever you want to call it, this is the function 
-        responsible for splitting the input up into tokens, one at a time.
-        
-        This is NOT the part responsible for handling the tokens. That part is 
-        in a file called driver.py.
+        This is what handles the actual 'compiling'. Rather, this function
+        creates the list of tokens that is executed by the driver.
         '''
-        
         text = self.text
+        pos = self.pos
         
-        # If the position is greater than the length of the input, return an EOF
-        # token.
-        if self.pos > len(text):
-            return Token(EOF, None)
-            
-        # The current character, used for determining what token to return.
-        currentChar = text[self.pos]
         
-        if currentChar.isDigit():
-            token = Token(INTEGER, int(currentChar))
-            self.tokenList.append(token)
-            self.pos += 1
-            return token
-            
-        if currentChar == '+':
-            token = Token(PLUS, currentChar)
-            self.tokenList.append(token)
-            self.pos += 1
-            return token
-            
-        if currentChar == '-':
-            token = Token(MINUS, currentChar)
-            self.tokenList.append(token)
-            self.pos += 1
-            return token
-            
-        if currentChar == '*':
-            token = Token(TIMES, currentChar)
-            self.tokenList.append(token)
-            self.pos += 1
-            return token
-            
-        if currentChar == '/':
-            token = Token(DIVIDE, currentChar)
-            self.tokenList.append(token)
-            self.pos += 1
-            return token
-            
-        if currentChar == '$':
-            token = Token(STACK_PREFIX, currentChar)
-            self.tokenList.append(token)
-            self.pos += 1
-            return token
-            
-        if currentChar == '<':
-            token = Token(SELECT_PREFIX, currentChar)
-            self.tokenList.append(token)
-            self.pos += 1
-            return token
-            
-        if currentChar == '=':
-            token = Token(ASSIGN, currentChar)
-            self.tokenList.append(token)
-            self.pos += 1
-            return token
-            
-        if currentChar == ';':
-            token = Token(END_OF_STATEMENT, currentChar)
-            self.tokenList.append(token)
-            self.pos += 1
-            return token
-            
-    def eat(self, tokenType):
-        if self.currentToken.type == tokenType:
-            self.token = self.getNextToken()
+        
+        
+    
+    def checkToken(self, tokenType):
+        if (self.currentToken.type == tokenType):
             return 1
         else:
             return 0
